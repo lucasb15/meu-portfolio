5f67f389d1e6a65fbecd8ba8f49b86fe0eb9956f
Lucas Gomes Santos <lgsantos306@gmail.com>
2023-07-17 19:24:22 -0300

    teste_vercel

--- src/Components/Sobre/Eu/index.js
+++ src/Components/Sobre/Eu/index.js
@@ -4,7 +4,7 @@ export default function Eu() {
     return (
         <div className="container meu_container">
             <h3 className="titulo">Sobre ( )</h3>
-            <p className="conteudo_sobre">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer velit turpis, finibus eu condimentum ut, posuere at ipsum. Maecenas vel consequat mauris. Quisque sodales ipsum aliquet aliquam placerat. Proin varius condimentum sapien. Mauris quis elit nibh. Morbi ut facilisis nisl. Duis et leo ut odio rhoncus porta a a dui. Nullam arcu nibh, bibendum in enim sed, ullamcorper feugiat nunc. </p>
+            <p className="conteudo_sobre">Testando atualização do vercel</p>
         </div>
     )
 }
