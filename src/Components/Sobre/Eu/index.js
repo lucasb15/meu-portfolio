6ea82c74d16316c393a770454bcc8b8d47b15feb
Lucas Gomes Santos <lgsantos306@gmail.com>
2023-07-17 20:06:44 -0300

    atualizacao_previa_sobre

--- src/Components/Sobre/Eu/index.js
+++ src/Components/Sobre/Eu/index.js
@@ -4,7 +4,7 @@ export default function Eu() {
     return (
         <div className="container meu_container">
             <h3 className="titulo">Sobre ( )</h3>
-            <p className="conteudo_sobre">Testando atualização do vercel</p>
+            <p className="conteudo_sobre">Cursando Engenharia Mecatronica, com experiencias em diversos setores como administrativo, fiscal e na manutenção eletrica e diesel. Busco minha colocação no mercado de tecnologia onde sempre tive bastante interesse, atualmente sou um desenvolvedor full stack em formação, durante meus estudos fiz alguns projetos, com o principal sendo o jogo JoKenPo, usando JS como linguagem de programação principal.</p>
         </div>
     )
 }
